a23b339ac45742d9a5b2a7a6247a61062e1e1f61
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-08-21 00:13:14 -0700

    got the console to jump to employee specific questions upon employee type selection. outlined new structure for inquirer section of proj

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ const Engineer = require('./employees/Engineer')
 const Intern = require('./employees/Intern')
 const inquirer = require('inquirer')
 
+console.log('create your team');
+
+// start with manager - every team has to have a manager 
+
+// add other members or finish team
+
+// engineer, intern, or done with team
+
+// add switch statement to cycle through prompt choices (as functions)
+
+// create function and prompt questions for switch statement 
+
+// add 'new Employee'
+
+// with saved employee infomation, make dynamic employee cards with js for html 
+
+// style employee cards w/ css
+
+
+
+
+
 function buildEmployee() {
   inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is the employees name', 
+      message: 'What is the employees name',
       validate: answer => {
-        if(answer !== '') {
+        if (answer !== '') {
           return true
         }
         return 'name is required'
@@ -20,7 +42,7 @@ function buildEmployee() {
       type: 'input',
       name: 'id',
       message: 'What is the employees id'
-    },   
+    },
     {
       type: 'input',
       name: 'email',
@@ -33,11 +55,15 @@ function buildEmployee() {
       choices: ['Manager', 'Engineer', 'Intern']
     },
   ]).then(answers => {
-    if(answers.type === 'Manager') {
-      let lastQuestion = inquirer.prompt({type: 'input', name: 'officeNumber', message: 'what is the managers office number'})
-      console.log(lastQuestion, answers);
+    if (answers.type === 'Manager') {
+      inquirer.prompt({ type: 'input', name: 'officeNumber', message: 'what is the managers office number' });
+    } else if (answers.type === 'Engineer') {
+      inquirer.prompt({ type: 'input', name: 'github', message: 'what is the engineers github username' });
+    } else {
+      inquirer.prompt({ type: 'input', name: 'school', message: 'what school does the intern attend' });
+      // console.log(lastQuestion, answers);
     }
-  })
+  });
 }
 
 buildEmployee()
